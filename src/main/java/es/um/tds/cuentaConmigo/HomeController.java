08ebb2552cdab1a74709beb17127fe2d9f109846
isalcode <isabelsalvic@gmail.com>
2025-11-08 14:09:22 +0100

    Editar en el filtro la parte de los meses para que sea multiple choice

--- src/main/java/es/um/tds/cuentaConmigo/HomeController.java
+++ src/main/java/es/um/tds/cuentaConmigo/HomeController.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 public class HomeController {
-	
-	@FXML
-    private ChoiceBox<String> cbMeses;
+
 
 
     @FXML
@@ -30,12 +28,6 @@ public class HomeController {
     }
 
     private void inicializarChoiceBoxes() {
-        // ChoiceBox Meses del año
-        cbMeses.getItems().addAll(
-            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-        );
-        cbMeses.getSelectionModel().selectFirst(); // seleccionar Enero por defecto
 
         // ChoiceBox Categorías desde el repositorio
         //List<Categoria> categorias = RepositorioCategorias.getCategorias(); // suponiendo tu método
